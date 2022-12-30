962091e960870505ca44a0d7c4063497c3915f00
Lion2517 <lion2517@yahoo>
2022-12-31 02:13:55 +0500

    200~Объектно-ориентированное программирование: ключевые принципы

---
+++ src/main/java/ru/netology/main/Main.java
@@ -0,0 +1,20 @@
+package ru.netology.main;
+
+import ru.netology.api.FormDate;
+import ru.netology.api.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.name = "Константин";
+        post.patronymic = "Константинович";
+        post.surname = "Рокоссовский";
+        post.birthday.day = 21;
+        post.birthday.month = 12;
+        post.birthday.year = 1896;
+        post.passport = "9999 № 999999";
+        post.phone = "+9 (999)-999-99-99";
+        post.subscription = true;
+    }
+}
